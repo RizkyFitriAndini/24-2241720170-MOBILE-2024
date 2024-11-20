5dd8a4c146e23fa0b6fe8ab190b9e1cf3a231f89
RizkyFitriAndini <fitriandini457@gmail.com>
2024-11-20 23:15:41 +0700

    week12

---
+++ pertemuan_11/Laporan.md
@@ -0,0 +1,21 @@
+<table>
+    <thead>
+        <th style="text-align: center;" colspan="2">Pertemuan 10</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nama</td>
+            <td>Rizky Fitri Andini</td>
+        </tr>
+        <tr>
+            <td>Nim</td>
+            <td>2241720170</td>
+        </tr>
+    </tbody>
+</table>
+
+# Pertemuan #11 | Pemrograman Asynchronous
+## Praktikum 1 
+Selesaikan langkah-langkah praktikum berikut ini menggunakan editor Visual Studio Code (VS Code) atau Android Studio atau code editor lain kesukaan Anda. Jawablah di laporan praktikum Anda pada setiap soal yang ada di beberapa langkah praktikum ini.
+### Langkah 1
+Buatlah sebuah project flutter baru dengan nama books di folder src week-11 repository GitHub Anda. Kemudian Tambahkan dependensi http dengan mengetik perintah berikut di terminal.
---
+++ pertemuan_11/src/books/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitri',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('GO!'),
+            onPressed: () {
+              setState(() {
+                isLoading = true;
+              });
+              Future.delayed(const Duration(seconds: 1)).then(((value) {
+                getData().then((value) {
+                  result = value.body.toString().substring(0, 450);
+                  setState(() {
+                    isLoading = false;
+                  });
+                }).catchError((e) {
+                  result = "An error occured $e";
+                  setState(() {
+                    isLoading = false;
+                  });
+                });
+              }));
+            },
+          ),
+          const Spacer(),
+          isLoading ? const CircularProgressIndicator() : Text(result),
+          const Spacer(),
+        ]),
+      ),
+    );
+  }
+}
+
+Future<Response> getData() async {
+  const authority = 'www.googleapis.com';
+  const path = '/books/v1/volumes/EoNqEAAAQBAJ';
+  Uri url = Uri.https(authority, path);
+  return await http.get(url);
+}
